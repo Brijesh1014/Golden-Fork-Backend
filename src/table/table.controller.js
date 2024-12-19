ab66bed6c2a15d1c9d4b12d781e6ff3fe9bdbb78
Brijesh Limbachiya <brijeshl.brainerhub@gmail.com>
2024-12-19 16:18:30 +0530

    Updated

--- src/table/table.controller.js
+++ src/table/table.controller.js
@@ -3,14 +3,19 @@ const Table_Model = require("./table.model");
 
 const createTable = async (req, res) => {
   try {
-    const { restaurantId, tableNumber, capacity,isAvailable } = req.body;
+    const { restaurantId, tableNumber, capacity, isAvailable } = req.body;
     let userId = req.userId;
 
     if (!restaurantId || !tableNumber) {
-      return res.status(400).json({ error: "Restaurant ID and tableNumber are required." });
+      return res
+        .status(400)
+        .json({ error: "Restaurant ID and tableNumber are required." });
     }
 
-    let existingTable = await Table_Model.findOne({ restaurantId, tableNumber });
+    let existingTable = await Table_Model.findOne({
+      restaurantId,
+      tableNumber,
+    });
 
     if (existingTable) {
       return res.status(400).json({
@@ -46,23 +51,21 @@ const createTable = async (req, res) => {
       restaurant: table,
     });
   } catch (error) {
-    res.status(500).json({ error: "Internal server error", details: error.message });
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
   }
 };
-
 
 const getAllTables = async (req, res) => {
   try {
     const { page = 1, limit = 10, restaurantName } = req.query;
 
-
     const pageNumber = parseInt(page);
     const pageSize = parseInt(limit);
     const skip = (pageNumber - 1) * pageSize;
 
-    const query = {};
-
-    let tablesQuery = Table_Model.find(query)
+    let tables = await Table_Model.find()
       .populate({
         path: "restaurantId",
         match: restaurantName
@@ -73,11 +76,7 @@ const getAllTables = async (req, res) => {
       .skip(skip)
       .limit(pageSize);
 
-    const tables = await tablesQuery.exec();
-
-    const filteredTables = tables.filter((table) => table.restaurantId !== null);
-
-    const totalTableCount = await Table_Model.countDocuments(query);
+    const totalTableCount = await Table_Model.countDocuments();
 
     const totalPages = Math.ceil(totalTableCount / pageSize);
     const remainingPages =
@@ -86,13 +85,13 @@ const getAllTables = async (req, res) => {
     return res.status(200).json({
       success: true,
       message: "Tables retrieved successfully",
-      tables: filteredTables,
+      tables,
       meta: {
-        totalTableCount: filteredTables.length,
+        totalTableCount: tables.length,
         currentPage: pageNumber,
         totalPages,
         remainingPages,
-        pageSize: filteredTables.length,
+        pageSize: tables.length,
       },
     });
   } catch (error) {
@@ -106,109 +105,111 @@ const getAllTables = async (req, res) => {
 };
 
 const getTableById = async (req, res) => {
-    try {
-      const { id } = req.params; 
-  
-      if (!id) {
-        return res.status(400).json({ error: "Table ID is required." });
-      }
-  
-      const table = await Table_Model.findById(id).populate("createdBy");
-      if (!table) {
-        return res.status(404).json({ error: "Table not found." });
-      }
-  
-      return res.status(200).json({
-        success: true,
-        message: "Table retrieved successfully.",
-        table,
-      });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "Internal server error", details: error.message });
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ error: "Table ID is required." });
     }
-  };
 
-  const updateTable = async (req, res) => {
-    try {
-      const { id } = req.params; 
-      const { restaurantId, tableNumber, capacity,isAvailable } = req.body;
-  
-      if (!id) {
-        return res.status(400).json({ error: "Table ID is required." });
-      }
-  
-      const table = await Table_Model.findById(id);
-      
-      if (!table) {
-        return res.status(404).json({ error: "Table not found." });
-      }
-
-      let existingTable = await Table_Model.findOne({ restaurantId:table.restaurantId, tableNumber });
-
-      if (existingTable) {
-        return res.status(400).json({
-          success: false,
-          message: `Table number ${tableNumber} already exists for this restaurant.`,
-        });
-      }
-  
-      if (restaurantId) table.restaurantId = restaurantId;
-      if (tableNumber) table.tableNumber = tableNumber;
-      if (capacity) table.capacity = capacity;
-      if (isAvailable !== undefined) table.isAvailable = isAvailable;
-  
-      await table.save();
-      return res.status(200).json({
-        success: true,
-        message: "Table updated successfully.",
-        table,
-      });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "Internal server error", details: error.message });
+    const table = await Table_Model.findById(id).populate("createdBy");
+    if (!table) {
+      return res.status(404).json({ error: "Table not found." });
     }
-  };
 
-  const deleteTable = async (req, res) => {
-    try {
-      const { id } = req.params;
-  
-      if (!id) {
-        return res.status(400).json({ error: "Table ID is required." });
-      }
-  
-      const table = await Table_Model.findByIdAndDelete(id);
-      if (!table) {
-        return res.status(404).json({ error: "Table not found." });
-      }
-  
-      const restaurant = await Restaurant_Model.findById(table.restaurantId);
-      if (restaurant) {
-        restaurant.tables = restaurant.tables.filter(
-          (tableId) => tableId.toString() !== id
-        );
-        await restaurant.save();
-      }
-  
-      return res.status(200).json({
-        success: true,
-        message: "Table deleted successfully.",
-      });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "Internal server error", details: error.message });
+    return res.status(200).json({
+      success: true,
+      message: "Table retrieved successfully.",
+      table,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
+};
+
+const updateTable = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { restaurantId, tableNumber, capacity, isAvailable } = req.body;
+
+    if (!id) {
+      return res.status(400).json({ error: "Table ID is required." });
     }
-  };
-    
+
+    const table = await Table_Model.findById(id);
+
+    if (!table) {
+      return res.status(404).json({ error: "Table not found." });
+    }
+
+    let existingTable = await Table_Model.findOne({
+      restaurantId: table.restaurantId,
+      tableNumber,
+    });
+
+    if (existingTable) {
+      return res.status(400).json({
+        success: false,
+        message: `Table number ${tableNumber} already exists for this restaurant.`,
+      });
+    }
+
+    if (restaurantId) table.restaurantId = restaurantId;
+    if (tableNumber) table.tableNumber = tableNumber;
+    if (capacity) table.capacity = capacity;
+    if (isAvailable !== undefined) table.isAvailable = isAvailable;
+
+    await table.save();
+    return res.status(200).json({
+      success: true,
+      message: "Table updated successfully.",
+      table,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
+};
+
+const deleteTable = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ error: "Table ID is required." });
+    }
+
+    const table = await Table_Model.findByIdAndDelete(id);
+    if (!table) {
+      return res.status(404).json({ error: "Table not found." });
+    }
+
+    const restaurant = await Restaurant_Model.findById(table.restaurantId);
+    if (restaurant) {
+      restaurant.tables = restaurant.tables.filter(
+        (tableId) => tableId.toString() !== id
+      );
+      await restaurant.save();
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Table deleted successfully.",
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
+};
+
 module.exports = {
-    createTable,
-    getAllTables,
-    getTableById,
-    updateTable,
-    deleteTable,
-
-}
+  createTable,
+  getAllTables,
+  getTableById,
+  updateTable,
+  deleteTable,
+};
